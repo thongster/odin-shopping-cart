bdb66bd3950b96449b08595f43bbebd97b28b5c1
thongster <106802301+thongster@users.noreply.github.com>
2025-12-17 06:13:20 +0700

    changed pages states over to react router instead

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { useState } from "react";
+import { Outlet } from "react-router";
 import "./styles/App.css";
-import Home from './pages/Home.jsx'
-import Shop from './pages/Shop.jsx'
-import Cart from './pages/Cart.jsx'
 
 function App() {
-  const [page, setPage] = useState("Home"); // Home, Shop, Cart
 
   return (
     <div className="container">
-      {page === "Home" && <Home />}
-      {page === "Shop" && <Shop />}
-      {page === "Cart" && <Cart />}
+      <Outlet />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,19 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import "./styles/modern-normalize.css";
 import "./styles/index.css";
 import App from "./App.jsx";
+import Home from './pages/Home.jsx'
+import Shop from './pages/Shop.jsx'
+import Cart from './pages/Cart.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {index: true, element: <Home /> },
+      {path: "shop", element: <Shop /> },
+      {path: "cart", element: <Cart /> },
+    ]
   },
 ]);
 
