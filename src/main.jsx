df1f4a7f896de293db1e87a02b3ca2956847092c
thongster <106802301+thongster@users.noreply.github.com>
2025-12-17 05:24:14 +0700

    modern normalize, remove template css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router";
+import './styles/modern-normalize.css'
 import './styles/index.css'
 import App from './App.jsx'
 
