2e80d888f828b3b1af42a756a91cb01b00f14fa6
thongster <106802301+thongster@users.noreply.github.com>
2025-12-18 06:36:21 +0700

    add error page

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,18 @@ import App from "./App.jsx";
 import Home from './pages/Home.jsx'
 import Shop from './pages/Shop.jsx'
 import Cart from './pages/Cart.jsx'
+import ErrorPage from "./pages/ErrorPage.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
     children: [
       {index: true, element: <Home /> },
       {path: "shop", element: <Shop /> },
       {path: "cart", element: <Cart /> },
-    ]
+    ],
   },
 ]);
 
