6316f4344141e2a044083b378f971670c500abd5
thongster <106802301+thongster@users.noreply.github.com>
2025-12-18 08:31:25 +0700

    switch to NavLink

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,19 @@
 import styles from "./Nav.module.css";
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 
 export default function Nav() {
+
   return (
     <div className={styles.nav}>
-      <Link to="/" className={styles.navLink}>
+      <NavLink to="/" className={styles.navLink}>
         Home
-      </Link>
-      <Link to="shop" className={styles.navLink}>
+      </NavLink>
+      <NavLink to="shop" className={styles.navLink}>
         Shop
-      </Link>
-      <Link to="cart" className={styles.navLink}>
+      </NavLink>
+      <NavLink to="cart" className={styles.navLink}>
         Cart
-      </Link>
+      </NavLink>
     </div>
   );
 }
