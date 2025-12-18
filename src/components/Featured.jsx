dc9ec597c1b2fe2827e00ce44094a6b629e2c030
thongster <106802301+thongster@users.noreply.github.com>
2025-12-19 04:07:06 +0700

    test displaying content

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -4,13 +4,17 @@ import styles from "./Featured.module.css";
 export default function Featured() {
   const { products, error, loading } = useProducts();
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>A network error was encountered</p>;
+
   return (
     <div className={styles.featured}>
       <div className={styles.featuredInner}>
         <h2 className={styles.featuredHeading}>Featured Drop</h2>
         <div className={styles.featuredGrid}>
           <div className={styles.featuredItem}>
-            {useProducts()}
+            
+            {products[0].title}
           </div>
           <div className={styles.featuredItem}></div>
           <div className={styles.featuredItem}></div>
