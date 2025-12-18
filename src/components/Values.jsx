5249498c525ca061b357f1e6c6b3f5d7536cc6b7
thongster <106802301+thongster@users.noreply.github.com>
2025-12-18 22:39:58 +0700

    styles componenent and css start

---
+++ src/components/Values.jsx
@@ -0,0 +1,11 @@
+import styles from './Values.module.css'
+
+export default function Values() {
+    return (
+        <div className={styles.values}>
+            <div className={styles.valuesInner}>
+                <h2 className={styles.valuesHeading}>Designed with intention.</h2>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero";
+import Values from "../components/Values";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <Values />
     </>
   );
 }
