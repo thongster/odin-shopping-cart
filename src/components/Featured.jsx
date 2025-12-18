ccda25f491e30b8691463504c1a12fb5797b73ee
thongster <106802301+thongster@users.noreply.github.com>
2025-12-19 00:15:46 +0700

    Featured components and basic style

---
+++ src/components/Featured.jsx
@@ -0,0 +1,24 @@
+import styles from './Featured.module.css'
+
+export default function Featured() {
+
+
+    return (
+        <div className={styles.featured}>
+            <div className={styles.featuredInner}>
+                <h2 className={styles.featuredHeading}>Featured Drop</h2>
+                <div className={styles.featuredGrid}>
+                <div className={styles.featuredItem}>
+
+                </div>
+                <div className={styles.featuredItem}>
+
+                </div>
+                <div className={styles.featuredItem}>
+
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
