8fa8ecf62c4c09866891ab1f426ca83570f3a916
thongster <106802301+thongster@users.noreply.github.com>
2025-12-18 07:34:16 +0700

    create Nav component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "./Header.module.css" 
+import Nav from "./Nav"
+
+export default function Header() {
+    return (
+        <header>
+            <h1>Logo</h1>
+            <Nav />
+        </header>
+    )
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import styles from "./Nav.module.css" 
+
+export default function Nav() {
+    return (
+        <div className="nav">
+            <button>Home</button>
+            <button>Shop</button>
+            <button>Cart</button>
+        </div>
+    )
+}
