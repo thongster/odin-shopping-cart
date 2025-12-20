77dcaa94573817a16b07a1944c83e4e218164d40
thongster <106802301+thongster@users.noreply.github.com>
2025-12-20 23:35:20 +0700

    Loading and Error components start

---
+++ src/components/Error.jsx
@@ -0,0 +1,16 @@
+import styles from 'LoadError.module.css'
+
+const Error = () => {
+    return (
+        <div className={styles.container}>
+            <div className={styles.containerInner}>
+                <div className={styles.state}>
+                <span className={styles.spinner} />
+                <p>Something went wrong.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Error
---
+++ src/components/Loading.jsx
@@ -0,0 +1,16 @@
+import styles from 'LoadError.module.css'
+
+const Loading = () => {
+    return (
+            <div className={styles.container}>
+            <div className={styles.containerInner}>
+                <div className={styles.state}>
+                <span className={styles.spinner} />
+                <p>Loading products…</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loading
