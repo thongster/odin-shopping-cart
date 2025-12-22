9efeb98f3a2cdcf5128bf9bf52a4e86e4137384c
thongster <106802301+thongster@users.noreply.github.com>
2025-12-23 00:43:41 +0700

    html for subtotal, tax, shipping, total, and check out button

--- src/components/CartTotal.jsx
+++ src/components/CartTotal.jsx
@@ -3,7 +3,44 @@ import { useOutletContext } from "react-router";
 
 const CartTotal = () => {
   const { cart } = useOutletContext();
-  return <></>;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerInner}>
+        <div className={styles.cartTotal}>
+          <h3>Order Summary</h3>
+          <div className={styles.summaryRows}>
+            <div className={styles.row}>
+              <span>Subtotal</span>
+              <span>$—.—</span>
+            </div>
+
+            <div className={styles.row}>
+              <span>Estimated Tax</span>
+              <span>$—.—</span>
+            </div>
+
+            <div className={styles.row}>
+              <span>Shipping</span>
+              <span>Free</span>
+            </div>
+
+            <div className={styles.divider} />
+
+            <div className={`${styles.row} ${styles.total}`}>
+              <span>Total</span>
+              <span>$—.—</span>
+            </div>
+
+            <button className={styles.checkoutBtn}>
+                Checkout
+            </button>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartTotal;
