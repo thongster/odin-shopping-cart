87a094f20dd779e7a932a7192cb5c2b8d5dfb8bf
thongster <106802301+thongster@users.noreply.github.com>
2025-12-23 00:09:47 +0700

    change formula to add all invidivudal quantities

--- src/components/CartCount.jsx
+++ src/components/CartCount.jsx
@@ -3,9 +3,11 @@ import styles from "./CartCount.module.css";
 const CartCount = ({ cart }) => {
   if (cart.length === 0) return null;
 
+  const allSum = cart.map((item) => item.count).reduce((a, b) => a + b, 0)
+
   return (
     <span className={styles.cartBadge}>
-      {cart.length}
+      {allSum}
     </span>
   );
 }
