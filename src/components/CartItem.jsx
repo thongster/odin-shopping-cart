192f0be9bab48bb487038348e2c03b8e540f83f2
thongster <106802301+thongster@users.noreply.github.com>
2025-12-22 07:11:55 +0700

    cart item image and details

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -3,7 +3,15 @@ import styles from "./CartItem.module.css";
 const CartItem = ({ item }) => {
   return (
     <div key={item.id} className={styles.cartItem}>
-      {item.title}
+        <img
+            src={item.image}
+            alt={item.title}
+            className={styles.image}
+        />
+  <div className={styles.details}>
+    <h3 className={styles.title}>{item.title}</h3>
+    <p className={styles.price}>${item.price.toFixed(2)}</p>
+  </div>
     </div>
   );
 };
