68998b4995ac85c9fdc01dd84342a53cd972bdf0
thongster <106802301+thongster@users.noreply.github.com>
2025-12-22 07:14:36 +0700

    added count state, buttons, and input to cart item

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,19 @@
+import { useState } from 'react'
+import { useOutletContext } from 'react-router';
 import styles from "./CartItem.module.css";
 
 const CartItem = ({ item }) => {
+  const { setCart, addToCart } = useOutletContext();
+  const [count, setCount] = useState(0);
+
+  const minusOne = () => {
+    if (count === 0) {
+      return;
+    } else {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <div key={item.id} className={styles.cartItem}>
         <img
@@ -8,10 +21,25 @@ const CartItem = ({ item }) => {
             alt={item.title}
             className={styles.image}
         />
-  <div className={styles.details}>
-    <h3 className={styles.title}>{item.title}</h3>
-    <p className={styles.price}>${item.price.toFixed(2)}</p>
-  </div>
+        <div className={styles.details}>
+            <h3 className={styles.title}>{item.title}</h3>
+            <p className={styles.price}>${item.price.toFixed(2)}</p>
+        </div>
+          <div className={styles.count}>
+            <button type="button" onClick={() => minusOne()}>
+              -
+            </button>
+            <input
+              type="number"
+              min="0"
+              aria-label="Quantity"
+              value={count}
+              onChange={(e) => setCount(e.target.value)}
+            />
+            <button type="button" onClick={() => setCount(count + 1)}>
+              +
+            </button>
+          </div>
     </div>
   );
 };
