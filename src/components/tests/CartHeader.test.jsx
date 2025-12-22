b01bc6d3ecd50d82282be208c2083a3eaff8e876
thongster <106802301+thongster@users.noreply.github.com>
2025-12-23 06:33:53 +0700

    cartheader test

---
+++ src/components/tests/CartHeader.test.jsx
@@ -0,0 +1,39 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen, fireEvent } from "@testing-library/react";
+import CartHeader from "../CartHeader";
+import { useOutletContext } from "react-router";
+
+vi.mock("react-router", async () => {
+  const actual = await vi.importActual("react-router");
+  return {
+    ...actual,
+    useOutletContext: vi.fn(),
+  };    
+})
+
+const setup = () => {
+  const clearCart = vi.fn();
+
+  useOutletContext.mockReturnValue({ clearCart });
+
+  render(<CartHeader />);
+
+  return { clearCart };
+};
+
+describe("CartHeader", () => {
+  it("renders the cart heading and description", () => {
+    setup();
+
+    expect(screen.getByText("Your Cart")).toBeInTheDocument();
+    expect(screen.getByText("Review items before checkout.")).toBeInTheDocument();
+  });
+
+  it("calls clearCart when Clear button is clicked", () => {
+    const { clearCart } = setup();
+
+    const clearBtn = screen.getByText(/clear/i)
+    fireEvent.click(clearBtn)
+    expect(clearCart).toHaveBeenCalled()
+  });
+});
