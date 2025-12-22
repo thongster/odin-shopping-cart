d6c9b31731bcc5548b2e61d21e4e042812f09564
thongster <106802301+thongster@users.noreply.github.com>
2025-12-23 04:49:10 +0700

    minor css edits, add logo to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
 import styles from "./Header.module.css";
+import logo from "../assets/logo.png"
 import Nav from "./Nav";
 
 export default function Header({ cart }) {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>After Dark Supply Co.</h1>
+      <div className={styles.logo}>
+        <img src={logo} alt="logo" />
+        <h1 className={styles.title}>After Dark Supply Co.</h1>
+      </div>
       <Nav cart={cart} />
     </header>
   );
