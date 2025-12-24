c6230bb2dadba6a2a5681609dd6f8eb89045cae8
thongster <106802301+thongster@users.noreply.github.com>
2025-12-24 12:48:47 +0700

    set up checkout successful component

---
+++ src/components/CheckoutDone.jsx
@@ -0,0 +1,11 @@
+import styles from 'CheckoutDone.module.css'
+
+const CheckoutDone = () => {
+  return (
+    <div className='checkoutDone'>
+      Checkout Successful!
+    </div>
+  )
+}
+
+export default CheckoutDone
